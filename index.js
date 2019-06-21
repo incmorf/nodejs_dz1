1de0da00fab82d7e7c16dbf20e639f0072bce21b
Henadiy Kuts <g.kuts@favbet.com>
2019-06-21 17:33:18 +0300

    http_fix

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const http = require('http');
 const timeOut = 5000;
+let interval;
 
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
   if (req.method === 'GET' && req.url !== '/favicon.ico') {
-    const interval = setInterval(() => {
-      console.log(new Date().toUTCString());
-    }, 1000);
+    let startTime = new Date();
+    let stopTime = startTime.setSeconds(startTime.getSeconds() + timeOut / 1000);
 
-    const timeout = (timeOut) => {
-      return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-          resolve();
-        }, timeOut);
-      });
-    };
-    timeout(timeOut).then(() => {
+    let waitForInterval = async () => new Promise(resolve => {
+      interval = setInterval(() => {
+        console.log(new Date().toUTCString());
+        let currentTime = new Date();
+
+        if (currentTime >= stopTime) {
+          console.log('Finish');
+          resolve(true);
+        }
+      }, 1000);
+    });
+    let intervalFinished = await waitForInterval();
+    if (intervalFinished) {
       clearInterval(interval);
       res.end(new Date().toUTCString());
-    });
+    }
   }
 }).listen(3000);
